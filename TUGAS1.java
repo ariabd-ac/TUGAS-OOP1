fc8543ae24cfdb7f2650d7b1740dbbe85cf656b1
Ari Abdurrahman Ghufron <48243245+ariabd-ac@users.noreply.github.com>
2019-03-13 19:05:24 +0700

    TUGAS OOP1-PERTEMUAN1

---
+++ TUGAS1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP1;
+
+/**
+ *
+ * @author Acer
+ */
+public class TUGAS1 {
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println("NAMA    = ARI ABDURRAHMAN GHUFRON");
+        System.out.println("Kelas   = 3C");
+        System.out.println("NIM     = 17090125");
+        System.out.println("==================================");
+    }
+}
